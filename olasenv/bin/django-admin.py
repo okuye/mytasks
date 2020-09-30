b91e96567edc99251a087761bf6f79ed7648682e
okuye <olakuye@gmail.com>
2020-09-30 18:38:02 -0400

    First Stage without templates implimented

---
+++ olasenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ok/Dev/Python/Projects/Django-Projects/mytasks/olasenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
