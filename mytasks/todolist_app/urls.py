eb4091bc7e44dfa81707d4307966826f329a1a53
okuye <olakuye@gmail.com>
2020-09-30 20:42:40 -0400

    Second Stage Before Models and Databases

--- mytasks/todolist_app/urls.py
+++ mytasks/todolist_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from todolist_app import views
 
 urlpatterns = [
-    path('',views.todolist,name='todolist'),
+    path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
--- mytasks/todolist_app/views.py
+++ mytasks/todolist_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def todolist(request):
-    return HttpResponse("Welcome to the Task Page")
+    content = {
+        'welcome_text': "Welcome From Jinja2.",
+    }
+    return render(request, 'todolist.html', content)
+
+
+def contact(request):
+    content = {
+        'contact_text': "Welcome to the contact page.",
+    }
+    return render(request, 'contact.html', content)
+
+
+def about(request):
+    content = {
+        'about_text': "Welcome to the about page.",
+    }
+    return render(request, 'about.html', content)
